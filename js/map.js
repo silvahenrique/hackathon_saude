0240cff59b58d5a38952e3a2542a88dff5ac70b8
henrique <henrique.contato@outlook.com>
2017-04-08 20:43:21 -0300

    Efetuando Rotas

---
+++ js/map.js
@@ -0,0 +1,111 @@
+(function () {
+var marker = null;
+var latlng = null;
+var API_KEY = "AIzaSyAefzKRkesd5paJ5myJCrhIp77SU0vDEac";
+
+var travelModes = [
+  { descrciption: "", mode: "DRIVING" },
+  { descrciption: "", mode: "BICYCLING" },
+  { descrciption: "", mode: "TRANSIT" },
+  { descrciption: "", mode: "WALKING" }
+];
+
+function exibirLocalizacaoUsuario(mapa, zoom) {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (posicao) {
+      latlng = {
+        lat: posicao.coords.latitude,
+        lng: posicao.coords.longitude
+      }
+
+      var marker = new google.maps.Marker({
+        position: latlng,
+        map: mapa
+      });
+
+      mapa.setCenter(latlng);
+      mapa.setZoom(zoom);
+    },
+
+    function () {
+      // LocalizacaoErro(true);
+    });
+   } else {
+     LocalizacaoErro(false);
+   }
+}
+
+function LocalizacaoErro(temGeocalizacao) {
+  temGeocalizacao ? alert("Tem") : alert("Não");
+}
+
+function localizarEndereco(idEntradaTexto, idObjetoClique, mapa, zoom) {
+  document.getElementById(idObjetoClique).addEventListener("click", function () {
+    var geocodificador = new google.maps.Geocoder();
+    var endereco = document.getElementById(idEntradaTexto).value;
+
+    geocodificador.geocode({ "address": endereco }, function (resultados, estado) {
+      if (estado == google.maps.GeocoderStatus.OK) {
+        var local = resultados[0].geometry.location;
+
+        mapa.setCenter(local);
+
+        var marker = new google.maps.Marker({
+          map: mapa,
+          position: local
+        });
+
+        mapa.setZoom(zoom);
+        mapa.panTo(local);
+      } else {
+        alert("Erro");
+      }
+    });
+  });
+}
+
+var mapa = new google.maps.Map(document.getElementById("mapa"), {
+  center: { lat: -23.533970, lng: -46.628870 },
+  zoom: 8,
+  panControl: true,
+  zoomControl: true,
+  zoomControlOptions: {
+    style: google.maps.ZoomControlStyle.LARGE,
+    position: google.maps.ControlPosition.RIGHT_CENTER
+  },
+  mapTypeControl: false,
+  scaleControl: false,
+  streetViewControl: false,
+  overviewMapControl: true,
+  rotateControl: true,
+  mapTypeId: google.maps.MapTypeId.ROADMAP
+});
+
+exibirLocalizacaoUsuario(mapa, 14)
+localizarEndereco("origem", "btnIr", mapa, 14);
+
+document.getElementById("btnRota").addEventListener("click", function () {
+  var directionsDisplay = new google.maps.DirectionsRenderer();
+  var directionsService = new google.maps.DirectionsService();
+
+  var start = document.getElementById('origem').value;
+  var end = document.getElementById('destino').value;
+
+  directionsDisplay.setMap(mapa);
+
+  var request = {
+    origin: start,
+    destination: end,
+    travelMode: 'DRIVING'
+  };
+
+  directionsService.route(request, function(result, status) {
+    if (status == 'OK') {
+      directionsDisplay.setDirections(result);
+
+      console.log(JSON.stringify(result));
+    }
+  });
+});
+
+})();
